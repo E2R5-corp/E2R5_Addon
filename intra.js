fe07929c3b7c7df43da1064af23db30ac0a949c3
Thibault GLANDAIS <tglandai@student.42.fr>
2019-01-02 18:13:00 +0100

    rm red button

--- intra.js
+++ intra.js
@@ -6,4 +6,7 @@
 
     var src = document.getElementsByClassName("name margin-top-15 margin-bottom-10")
     src[0].append(img)
+    
+    document.getElementsByClassName("help-button")[0].remove()
+    document.getElementsByClassName("ol-is-watching-you")[0].remove()
 })();
