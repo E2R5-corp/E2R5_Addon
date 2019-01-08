334a153e59103b4d9d4a5487e058371a13f4d68e
Thibault GLANDAIS <tglandai@student.42.fr>
2019-01-08 16:32:45 +0100

    fix images

--- stud42.js
+++ stud42.js
@@ -3,16 +3,27 @@
     img.src = "https://i.imgur.com/WOmX9KB.png"
     img.width = 210
     img.height = 210
-    
-    var src = document.getElementsByClassName("credits")
-    src[0].append(img)
-    
+        
     var img2 = document.createElement("img")
     img2.src = "https://i.imgur.com/WOmX9KB.png"
     img2.width = 50
     img2.height = 50
+
+    var img3 = document.createElement("img")
+    img3.src = "https://i.imgur.com/WOmX9KB.png"
+    img3.width = 50
+    img3.height = 50
+
+    var src = document.getElementsByClassName("credits")
+    src[0].prepend(img)
+    
     var clusters = document.getElementsByTagName("tbody")
-    var r5 = clusters[1].children[8].children[0]
-    r5.textContent = ""
-    r5.append(img2)
+
+    var r5right = clusters[1].children[8].children[0]
+    r5right.textContent = ""
+    r5right.append(img2)
+
+    var r5left = clusters[1].children[8].children[26]
+    r5left.textContent = ""
+    r5left.append(img3)
 })();
