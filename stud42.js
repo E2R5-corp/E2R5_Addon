e65f1ed43c06cf8bc6441d48696952bb7380b8ae
anroche <anroche@student.42.fr>
2019-08-11 15:59:46 +0200

    update popup link

--- stud42.js
+++ stud42.js
@@ -14,6 +14,16 @@
     img3.width = 50
     img3.height = 50
 
+    var img4 = document.createElement("img")
+    img4.src = "https://i.imgur.com/X1QBzRU.png"
+    img4.width = 50
+    img4.height = 50
+
+    var img5 = document.createElement("img")
+    img5.src = "https://i.imgur.com/X1QBzRU.png"
+    img5.width = 50
+    img5.height = 50
+
     var src = document.getElementsByClassName("credits")
     src[0].prepend(img)
     
@@ -26,6 +36,14 @@
     var r5left = clusters[1].children[8].children[26]
     r5left.textContent = ""
     r5left.append(img3)
+
+    var r5midleft = clusters[1].children[8].children[8]
+    r5midleft.textContent = ""
+    r5midleft.append(img4)
+
+    var r5midright = clusters[1].children[8].children[17]
+    r5midright.textContent = ""
+    r5midright.append(img5)
 
     var location = document.getElementsByClassName("connected")[0].children[1].children[0]
     if (location.textContent.match(/e2r[4-6]/g))
