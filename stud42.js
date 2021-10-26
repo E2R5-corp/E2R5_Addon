c0f35ec62807f561464589629b23cfaa481ef1be
anroche <anroche@student.42.fr>
2021-10-27 01:41:03 +0200

    Manifest v3 / update members

--- stud42.js
+++ stud42.js
@@ -1,6 +1,6 @@
 (function () {
-    document.getElementsByClassName("sidebar-menu-inner")[0].getElementsByTagName("a")[6].remove()
-
+   document.getElementsByClassName("sidebar-menu-inner") && document.getElementsByClassName("sidebar-menu-inner")[0].getElementsByTagName("a")[6].remove()
+   
     var img = document.createElement("img")
     img.src = "https://i.imgur.com/1vtZlTB.png"
     img.width = 160
@@ -16,16 +16,6 @@
     img3.width = 50
     img3.height = 50
 
-    var img4 = document.createElement("img")
-    img4.src = "https://i.imgur.com/X1QBzRU.png"
-    img4.width = 50
-    img4.height = 50
-
-    var img5 = document.createElement("img")
-    img5.src = "https://i.imgur.com/X1QBzRU.png"
-    img5.width = 50
-    img5.height = 50
-
     var src = document.getElementsByClassName("credits")
     src[0].prepend(img)
 
@@ -39,18 +29,14 @@
     r5left.textContent = ""
     r5left.append(img3)
 
-    var r5midleft = clusters[1].children[8].children[8]
-    r5midleft.textContent = ""
-    r5midleft.append(img4)
-
-    var r5midright = clusters[1].children[8].children[18]
-    r5midright.textContent = ""
-    r5midright.append(img5)
-
     var location = document.getElementsByClassName("connected")[0].children[1].children[0]
-    if ( location.textContent.match(/e2r[4-6]/g))
+
+    if (location) 
+    {
+        if ( location.textContent.match(/e2r[4-6]/g))
         location.style.color = "green"
-    else
-        location.style.color = "red"
+        else
+            location.style.color = "red"
+    }
 
 })();
